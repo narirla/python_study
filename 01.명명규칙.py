5f1f59e16864acda3d880e50c8b2794128aceb7e
kim nari <knri0320@gmail.com>
2025-11-05 23:17:26 +0900

    파이썬

---
+++ 01.명명규칙.py
@@ -0,0 +1,16 @@
+# a-z, A-Z, 0-9, _ ,한글
+# 명명규칙:변수명, 함수명, 클래스명
+# ctrl +s
+# abcA = 10
+#  ab_cd = 30
+# 숫자로 시작 X
+# 3abc = 40 (X)
+# 키워드(예약어: if, while, for, class, def, ...)
+# if = 50 X
+# ab$cd = 60 X
+a = 10
+숫자 = 20
+print( a )
+print(숫자)
+print( bin(a))
+print( type(a) )
